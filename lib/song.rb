a1eb75e369f1651e5cb92b7950ad844385be3f3b
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-06-10 00:44:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,7 @@ class Song
   def self.new_by_filename(file_name)
     file = file_name.split(" - ")
     song = self.new(file[1])
-   song.artist = file[0]
+   song.artist_name = file[0]
    song
   end
   
