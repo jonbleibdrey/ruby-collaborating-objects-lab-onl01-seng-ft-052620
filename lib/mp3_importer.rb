5251b52085f49133ebeefda884b21f22d83d4d3d
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-06-10 00:04:17 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,13 +1,15 @@
 class MP3Importer
   
-  attr_accessor :path
+  attr_accessor :path :files
   
   
   def initialize(file_path)
     @path = file_path 
+    @files = []
+    Dir.foreach(@path) do |file|
+      if file.include?("mp3")
+        @files << file
+      end
   end
   
-  def files
-    Dir.foreach(@path)
-  end
 end
