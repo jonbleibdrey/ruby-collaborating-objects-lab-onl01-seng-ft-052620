4c5d94de440c0212614efade5cbcac2b7c5f9721
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-06-10 00:15:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,10 @@ class Song
     @@all << self
   end
   
+  def self.new_by_file(file_name)
+    file = file_name.split(" - ")
+    song = self.new(file[1])
+   song.artist = file[0]
+  end
+  
 end
